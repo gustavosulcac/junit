011479e44bf14ed990b54cee93f5aab9f5637859
Gustavo Sulca <gustavosulcac@gmail.com>
2019-01-27 17:21:38 -0500

    configuracion inicial

---
+++ junit/src/test/java/pe/codemy/junit/JunitApplicationTests.java
@@ -0,0 +1,17 @@
+package pe.codemy.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JunitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
